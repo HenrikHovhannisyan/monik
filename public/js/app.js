ed4f34136262be9ae9c23620954d664c71330b07
HenrikHovhannisyan <henrik.hovhannisyan.91@gmail.com>
2024-04-30 14:11:19 +0400

    Fixed bugs for multi language

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const dropdownItems = document.querySelectorAll('.select-lang');
+    dropdownItems.forEach(item => {
+        item.addEventListener('click', function () {
+            const selectedLanguage = this.getAttribute('data-lang');
+            localStorage.setItem('selectedLanguage', selectedLanguage);
+        });
+    });
 
+    const selectedLanguage = localStorage.getItem('selectedLanguage');
+    if (selectedLanguage && selectedLanguage !== "null" && window.location.pathname.slice(1, 3) !== selectedLanguage) {
+        window.location.href = `/${selectedLanguage}`;
+    }
+});
