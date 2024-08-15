045b42523f1a78f9c3e2204d798be2cb74484475
HenrikHovhannisyan <henrik.hovhannisyan.91@gmail.com>
2024-08-15 18:09:10 +0400

    Fixed bug

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -18,14 +18,14 @@
     {{--  Styles  --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
           crossorigin="anonymous">
-    <link rel="stylesheet" href="{{asset('css/admin.css')}}">
+    <link rel="stylesheet" href="{{asset('css/admin.css?v=' . time()) }}">
 
     <!-- Scripts -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
             crossorigin="anonymous"></script>
     <script src="https://cdn.ckeditor.com/ckeditor5/40.1.0/classic/ckeditor.js"></script>
-    <script defer src="{{asset('js/admin.js')}}"></script>
+    <script defer src="{{asset('js/admin.js?v=' . time()) }}"></script>
 </head>
 <body>
 <div id="app">
