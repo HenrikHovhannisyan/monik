96345c4bddfb3057c5375dfa8b8386f4905e5587
HenrikHovhannisyan <henrik.hovhannisyan.91@gmail.com>
2025-07-04 12:59:28 +0400

    Added Minification

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,45 +6,47 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta content="Henrik" name="author" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta
-        name="description"
-        content="Shopwise is Powerful features and You Can Use The Perfect Build this Template For Any eCommerce Website. The template is built for sell Fashion Products, Shoes, Bags, Cosmetics, Clothes, Sunglasses, Furniture, Kids Products, Electronics, Stationery Products and Sporting Goods."
-    />
-    <meta
-        name="keywords"
-        content="ecommerce, electronics store, Fashion store, furniture store,  bootstrap 4, clean, minimal, modern, online store, responsive, retail, shopping, ecommerce store"
-    />
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <!-- Connecting the manifest -->
+    <meta name="description" content="Shopwise is a powerful eCommerce template for various products including fashion, electronics, furniture, and more." />
+    <meta name="keywords" content="ecommerce, fashion, furniture, electronics, responsive, modern, online store" />
+
+    <!-- PWA -->
     <link rel="manifest" href="{{ asset('manifest.json') }}">
     <meta name="theme-color" content="#202325">
-
-    <!-- Support for iOS -->
-    <link rel="apple-touch-icon" href="{{ asset('images/pwa/icon-192x192.png?v=' . time()) }}">
+    <link rel="apple-touch-icon" href="{{ asset('images/pwa/icon-192x192.png') }}">
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- Title & Favicon -->
     <title>{{ config('app.name', 'Monik') }} @yield('title')</title>
-    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('favicon.ico?v=' . time()) }}"/>
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('favicon.ico') }}"/>
 
-    <!-- Google Font -->
-    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900&display=swap" rel="stylesheet"/>
-    <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800,900&display=swap" rel="stylesheet"/>
     <!-- Fonts -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" crossorigin="anonymous" referrerpolicy="no-referrer"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap">
+    <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800;900&display=swap">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet" media="all">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" media="all">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512..." crossorigin="anonymous" referrerpolicy="no-referrer" />
+
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
-    <!-- Style -->
-    <link rel="stylesheet" href="{{ asset('css/site.min.css?v=' . time()) }}">
 
-    <!-- Yandex.Metrika counter -->
-    <script type="text/javascript" >
-        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+    <!-- Main CSS (Laravel Mix) -->
+    <link rel="stylesheet" href="{{ mix('css/site.min.css') }}">
+
+    <!-- Yandex.Metrika -->
+    <script type="text/javascript" defer>
+        (function(m,e,t,r,i,k,a){
+            m[i]=m[i]||function(){ (m[i].a=m[i].a||[]).push(arguments) };
             m[i].l=1*new Date();
-            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+            for (let j = 0; j < document.scripts.length; j++) {
+                if (document.scripts[j].src === r) return;
+            }
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a);
+        })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
         ym(98547249, "init", {
             clickmap:true,
@@ -55,9 +57,8 @@
         });
     </script>
     <noscript><div><img src="https://mc.yandex.ru/watch/98547249" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-    <!-- /Yandex.Metrika counter -->
-
 </head>
+
 <body>
     <div id="app">
         @include('vendor.modal.ad')
@@ -66,25 +67,31 @@
         @include('vendor.modal.success-message')
         @include('vendor.modal.products-filter')
         @include('layouts.header')
+
         <main>
             @yield('content')
         </main>
-        @include('layouts.footer')
 
+        @include('layouts.footer')
 
         <div id="successMessage" class="message success hidden">
             {{ __("index.copied_sku") }}: <span id="copiedSKU"></span>
         </div>
+
         <a href="#" class="scrollup" style="display: none">
             <i class="fa-solid fa-chevron-up"></i>
         </a>
     </div>
-    <!--  Bootstrap -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-    <!-- yandex js -->
-    <script src="https://api-maps.yandex.ru/2.1/?lang={{ App::getLocale() === 'am' ? 'hy' : App::getLocale() }}&apikey=69788c43-a05a-4647-a75e-a9e4f7625d6f&?v={{ time() }}" type="text/javascript"></script>
-    <!-- Script -->
-    <script src="{{ asset('js/site.min.js?v=' . time()) }}" defer></script>
+
+    <!-- JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" defer crossorigin="anonymous"></script>
+
+    <!-- Yandex Maps -->
+    <script src="https://api-maps.yandex.ru/2.1/?lang={{ App::getLocale() === 'am' ? 'hy' : App::getLocale() }}&apikey=69788c43-a05a-4647-a75e-a9e4f7625d6f" defer></script>
+
+    <!-- Main JS (Laravel Mix) -->
+    <script src="{{ mix('js/site.min.js') }}" defer></script>
+
     @yield('script')
 </body>
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,42 +1,62 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
-mix.styles([
-    'public/css/animate.css',
-    'public/css/all.min.css',
-    'public/css/ionicons.min.css',
-    'public/css/themify-icons.css',
-    'public/css/linearicons.css',
-    'public/css/simple-line-icons.css',
-    'public/owlcarousel/css/owl.carousel.min.css',
-    'public/owlcarousel/css/owl.theme.css',
-    'public/owlcarousel/css/owl.theme.default.min.css',
-    'public/css/magnific-popup.css',
-    'public/css/jquery-ui.css',
-    'public/css/slick.css',
-    'public/css/slick-theme.css',
-    'public/css/root.css',
-    'public/css/main.css',
-    'public/css/style.css',
-    'public/css/responsive.css',
-], 'public/css/site.css');
+mix.styles(
+    [
+        "public/css/animate.css",
+        "public/css/all.min.css",
+        "public/css/ionicons.min.css",
+        "public/css/themify-icons.css",
+        "public/css/linearicons.css",
+        "public/css/simple-line-icons.css",
+        "public/owlcarousel/css/owl.carousel.min.css",
+        "public/owlcarousel/css/owl.theme.css",
+        "public/owlcarousel/css/owl.theme.default.min.css",
+        "public/css/magnific-popup.css",
+        "public/css/jquery-ui.css",
+        "public/css/slick.css",
+        "public/css/slick-theme.css",
+        "public/css/root.css",
+        "public/css/main.css",
+        "public/css/style.css",
+        "public/css/responsive.css",
+    ],
+    "public/css/site.css"
+);
 
-mix.scripts([
-    'public/js/jquery-3.7.1.min.js',
-    'public/js/jquery-ui.js',
-    'public/js/popper.min.js',
-    'public/owlcarousel/js/owl.carousel.min.js',
-    'public/js/magnific-popup.min.js',
-    'public/js/waypoints.min.js',
-    'public/js/parallax.js',
-    'public/js/jquery.countdown.min.js',
-    'public/js/imagesloaded.pkgd.min.js',
-    'public/js/isotope.min.js',
-    'public/js/slick.min.js',
-    'public/js/jquery.elevatezoom.js',
-    'public/js/map.js',
-    'public/js/scripts.js',
-    'public/js/app.js',
-], 'public/js/site.js');
+mix.scripts(
+    [
+        "public/js/jquery-3.7.1.min.js",
+        "public/js/jquery-ui.js",
+        "public/js/popper.min.js",
+        "public/owlcarousel/js/owl.carousel.min.js",
+        "public/js/magnific-popup.min.js",
+        "public/js/waypoints.min.js",
+        "public/js/parallax.js",
+        "public/js/jquery.countdown.min.js",
+        "public/js/imagesloaded.pkgd.min.js",
+        "public/js/isotope.min.js",
+        "public/js/slick.min.js",
+        "public/js/jquery.elevatezoom.js",
+        "public/js/map.js",
+        "public/js/scripts.js",
+        "public/js/app.js",
+    ],
+    "public/js/site.js"
+);
 
-mix.minify('public/css/site.css');
-mix.minify('public/js/site.js');
+// Минификация
+mix.minify("public/css/site.css");
+mix.minify("public/js/site.js");
+
+// Настройки минификации (удаление комментариев и прочее)
+mix.options({
+    terser: {
+        extractComments: false, // удаляем все комментарии из JS
+    },
+    cssNano: {
+        discardComments: { removeAll: true }, // удаляем все комментарии из CSS
+    },
+});
+
+// Версионирование для автоматического кеширования
+mix.version();
